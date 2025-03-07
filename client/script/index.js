7eea68454a63883344e93d2c000ada9645855189
Ashish Gupta <gkashish1985@gmail.com>
2025-03-07 23:35:25 +0530

    update prod

--- client/script/index.js
+++ client/script/index.js
@@ -2,7 +2,7 @@ document.getElementById("get-greeting").addEventListener("click", async () => {
     console.log("Button clicked");
     try {
         const name = document.getElementById("name").value;
-        const response = await fetch(`http://localhost:5000/api/v1/greet?name=${name}`);
+        const response = await fetch(`https://server-gamma-murex.vercel.app/api/v1/greet?name=${name}`);
         const data = await response.json();
         console.log(data);
         document.getElementById("message").innerText = data.message;
--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const greetRouter = require("./routes/greet-route");
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
